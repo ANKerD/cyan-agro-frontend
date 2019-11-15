a647cb3dfe3b67147329f1363a624ba837e9f3c8
Anderson Dantas <a.kleber.d@gmail.com>
2019-11-15 16:23:56 -0300

    Updates

---
+++ precache-manifest.2332ac920060b70fd7769f8cadfa259f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe03760ec9566801dc29c7757cc8480",
+    "url": "/cyan-agro-frontend/index.html"
+  },
+  {
+    "revision": "4dec5a742c1117cc091f",
+    "url": "/cyan-agro-frontend/static/js/2.c73e9bff.chunk.js"
+  },
+  {
+    "revision": "1b546eb932509f857f0f",
+    "url": "/cyan-agro-frontend/static/js/main.e998c258.chunk.js"
+  },
+  {
+    "revision": "2262969debefdd213250",
+    "url": "/cyan-agro-frontend/static/js/runtime-main.652624d9.js"
+  }
+]);
